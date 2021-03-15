5319b0f29181d28ed6eb1fea26362f610c3e3664
sassok <kevin.sassoo@gmail.com>
2021-03-15 15:26:37 +0100

    seed and fix event table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,87 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_03_15_131548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "add_column_agencies", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "add_column_owners", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "agencies", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "city"
+    t.string "adress"
+    t.string "phone_number"
+    t.string "siren"
+    t.index ["email"], name: "index_agencies_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_agencies_on_reset_password_token", unique: true
+  end
+
+  create_table "buildings", force: :cascade do |t|
+    t.bigint "agency_id"
+    t.string "adress"
+    t.string "name"
+    t.string "reference"
+    t.string "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["agency_id"], name: "index_buildings_on_agency_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.bigint "building_id"
+    t.datetime "datetime"
+    t.integer "duration"
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["building_id"], name: "index_events_on_building_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "owner_id"
+    t.bigint "building_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["building_id"], name: "index_messages_on_building_id"
+    t.index ["owner_id"], name: "index_messages_on_owner_id"
+  end
+
+  create_table "owners", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "building_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.integer "lot"
+    t.index ["building_id"], name: "index_owners_on_building_id"
+    t.index ["email"], name: "index_owners_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "owners", "buildings"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Agency.create(email: "1test@test.com", password: "testtest", name: "agence1")
+Agency.create(email: "2test@test.com", password: "testtest", name: "agence2")
+Agency.create(email: "3test@test.com", password: "testtest", name: "agence3")
+
+
+Building.create(name: "Building1", agency_id: 1)
+Building.create(name: "Building2", agency_id: 3)
+Building.create(name: "Building3", agency_id: 1)
+Building.create(name: "Building4", agency_id: 2)
+Building.create(name: "Building5", agency_id: 3)
+Building.create(name: "Building6", agency_id: 1)
+
+
+Owner.create(building_id: 2, first_name: "Michel", email: "test@gmail.com", password: "testtest")
+Owner.create(building_id: 2, first_name: "Rodrigo", email: "test1@gmail.com", password: "testtest")
+Owner.create(building_id: 2, first_name: "Marguerite", email: "test2@gmail.com", password: "testtest")
+Owner.create(building_id: 3, first_name: "Josiane", email: "test3@gmail.com", password: "testtest")
+Owner.create(building_id: 3, first_name: "Martin", email: "test4@gmail.com", password: "testtest")
+Owner.create(building_id: 5, first_name: "Lola", email: "test5@gmail.com", password: "testtest")
+Owner.create(building_id: 4, first_name: "Youpi", email: "testu@gmail.com", password: "testtest")
+Owner.create(building_id: 4, first_name: "Rodriga", email: "testz@gmail.com", password: "testtest")
+Owner.create(building_id: 2, first_name: "MargueritO2", email: "teste@gmail.com", password: "testtest")
+Owner.create(building_id: 1, first_name: "Louis", email: "testr@gmail.com", password: "testtest")
+Owner.create(building_id: 6, first_name: "Max", email: "testa@gmail.com", password: "testtest")
+Owner.create(building_id: 5, first_name: "Lil Wayne", email: "testf@gmail.com", password: "testtest")
+
+
+
+
+Message.create(owner_id: 4, building_id: 3, content: "bonjour les voisins")
+Message.create(owner_id: 2, building_id: 2, content: "bonjour les voisins")
+Message.create(owner_id: 2, building_id: 2, content: "bonjour les voisins")
+Message.create(owner_id: 10, building_id: 1, content: "bonjour les voisins")
+Message.create(owner_id: 10, building_id: 1, content: "bonjour les voisins")
+Message.create(owner_id: 10, building_id: 1, content: "bonjour les voisins")
+Message.create(owner_id: 1, building_id: 2, content: "bonjour les voisins")
+Message.create(owner_id: 1, building_id: 2, content: "bonjour les voisins")
+Message.create(owner_id: 1, building_id: 2, content: "bonjour les voisins")
+
+Event.create(building_id: 1, title: "Assemblée générale")
+Event.create(building_id: 2, title: "Assemblée générale")
+Event.create(building_id: 3, title: "Assemblée générale")
+Event.create(building_id: 4, title: "Assemblée générale")
+Event.create(building_id: 5, title: "Assemblée générale")
+Event.create(building_id: 6, title: "Assemblée générale")
