acd1505db90ff05adcf6e895e60b14d41f645803
anthopayan <anthony.payan08@outlook.fr>
2021-03-15 14:12:49 +0100

    add_db_events

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,4 +1,5 @@
 class Building < ApplicationRecord
     belongs_to :agency
     has_many :owners
+    has_many :events
 end
---
+++ db/migrate/20210315125244_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :events do |t|
+      t.belongs_to :building, index: true
+      t.string :content
+      t.datetime :datetime
+      t.integer :duration
+      t.string :title
+      t.string :description
+      t.timestamps
+    end
+  end
+end
