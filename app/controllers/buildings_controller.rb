f7500612edc1bd0d62d675c3021b3804fa3fa0bc
sassok <kevin.sassoo@gmail.com>
2021-03-16 16:57:12 +0100

    page home agencies work

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,4 +1,10 @@
 class BuildingsController < ApplicationController
+
+    def index 
+        @buildings = Building.all
+        @building = Building.find_by(agency_id: params[:agency_id])
+    end 
+
     def create
     end
 end
