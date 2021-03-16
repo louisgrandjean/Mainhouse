000067f975f3385a2346855e39c4aef92dd567ca
sassok <kevin.sassoo@gmail.com>
2021-03-16 17:19:14 +0100

    good indent for views index buildings

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -2,7 +2,6 @@ class BuildingsController < ApplicationController
 
     def index 
         @buildings = Building.all
-        @building = Building.find_by(agency_id: params[:agency_id])
     end 
 
     def create
