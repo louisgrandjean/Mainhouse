3ee81830bf79aaebcff3e13329e493211acf73c4
volpito <mpj.rigot@gmail.com>
2021-03-16 09:22:54 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,6 @@ Rails.application.routes.draw do
   resources :owners#, :path => "proprietaire"
   resources :events#, :path => "evenement"
   resources :messages#, :path => "discussions"
-
-  # a terme : mettre la root vers la _static ou _landing, non ?
-  root to: "buildings#index"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
