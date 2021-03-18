84c30db35937a146a285314b55b9504f36fc0b73
volpito <mpj.rigot@gmail.com>
2021-03-18 13:10:09 +0100

    CRUD Agencies, show agencies, edit agencies, edit registration agencies

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -1,10 +1,32 @@
 class AgenciesController < ApplicationController
 
-def index 
-  @building = Building.find_by(agence_id: (params[:id]))
+def show 
+  @agency = Agency.find(params[:id])
 end 
 
 def create
+end
+
+def edit
+  @agency = Agency.find(params[:id])
+end
+
+
+def update
+  @agency = Agency.find(params[:id])
+  @agency.update(agency_params)
+  redirect_to agency_path(params[:id])
+end
+
+def destroy
+  @agency = Agency.find_by(id: params[:id])
+  @agency.destroy
+  redirect_to root_path
+end
+
+private
+def agency_params
+  params.permit(:name, :city, :adress, :phone_number, :siren)
 end
 
 end
