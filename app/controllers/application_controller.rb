09bc06eadb1117ef4060bd4b7558da37d4b197ef
sassok <kevin.sassoo@gmail.com>
2021-03-19 10:09:57 +0100

    redirection after sign up sign in agency

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  
+  def after_sign_in_path_for(resource)
+    case resource
+    when Agency
+      agency_buildings_path(current_agency)
+    when Owner
+      root_path
+    end
+  end
+
 end
