78ff8f8a52d0cbdd3bdf343cc0234ffadf3e3a72
volpito <mpj.rigot@gmail.com>
2021-03-20 18:50:07 +0100

    show owner in views & in CRUD

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,4 +1,17 @@
 class OwnersController < ApplicationController
+    def show
+			@owner = Owner.find_by(building_id: params[:id], id: current_owner)
+			@building = Building.find_by(agency_id: current_agency, id: params[:id])
+			@agency = Agency.find_by(id: current_agency)
+
+			if @owner.nil?
+				redirect_to new_owner_session_path
+			end
+    end
+    
+		def index
+		end
+
     def create
     end
 end
