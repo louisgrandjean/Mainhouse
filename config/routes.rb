edac423e079294bb16fe7cf1d22a930b55339fc2
anthopayan <anthony.payan08@outlook.fr>
2021-03-21 17:44:25 +0100

    add_events_agency

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   resources :messages#, :path => "discussions"
   root :to => "landing_pages#index"
   get '/agencies/:agency_id/owners', to: 'owners#owners', as: 'all_owners'
+  get '/agencies/:agency_id/events', to: 'events#events', as: 'all_events'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
